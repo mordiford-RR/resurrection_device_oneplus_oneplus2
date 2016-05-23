639541867b07abd22b55169113810d51ec8c1516
Steve Kondik <steve@cyngn.com>
2016-05-22 19:52:13 -0700

    op2: Add blobs for IPv6 tethering
    Change-Id: Ie236f70a32eb471d443f4a6678ba5696954c173d
    
    Conflicts:
    	proprietary-files.txt
    	setup-makefiles.sh

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -67,6 +67,9 @@ PRODUCT_PACKAGES += \\
     TimeService
 
 PRODUCT_PACKAGES += \\
+    fastdormancy \\
+    QtiTelephonyService \\
+    QtiTetherService \\
     qcnvitems \\
     qcrilhook
 
@@ -171,6 +174,17 @@ LOCAL_SRC_FILES := proprietary/app/shutdownlistener/shutdownlistener.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := QtiTetherService
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/priv-app/QtiTetherService/QtiTetherService.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
 LOCAL_CERTIFICATE := platform
 include \$(BUILD_PREBUILT)
 
